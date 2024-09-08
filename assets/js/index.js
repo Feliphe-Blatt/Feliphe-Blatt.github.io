e4fa83e24d6d226df62db077eb0a08e4ad883477
Feliphe S. Blatt <felipheblatt@gmail.com>
2024-09-08 10:34:49 -0300

    Feature(v16): Preparando para adição de splide

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
-function escopo() {
-
-    
-    
-}
-
-escopo();
+// iniciar splide sliders de index.html
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+        type: 'loop',
+        perPage: 1,
+        autoplay: true,
+        interval: 1000,
+        pauseOnHover: true,
+        arrows: false,
+        pagination: true,
+    }).mount();
+});
