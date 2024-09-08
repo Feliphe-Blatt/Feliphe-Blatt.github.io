59da0a1d49b0a5b544a4e5523e63ca18f786698f
Feliphe S. Blatt <felipheblatt@gmail.com>
2024-09-08 16:42:45 -0300

    Feature(v16): Aplicando Splide slides na sessão de projetos...

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,13 @@
-// iniciar splide sliders de index.html
-document.addEventListener('DOMContentLoaded', function () {
-    new Splide('.splide', {
-        type: 'loop',
-        perPage: 1,
-        autoplay: true,
-        interval: 1000,
-        pauseOnHover: true,
-        arrows: false,
-        pagination: true,
-    }).mount();
-});
+document.addEventListener( 'DOMContentLoaded', function () {
+
+  new Splide('#splide-slider-sobre', {
+    type: 'loop',
+    arrows: true,
+    gap: '1rem',
+    keyboard: 'global',
+    wheel: false,
+    pagination: true,
+    perPage: 1,
+  }).mount();
+
+} );
